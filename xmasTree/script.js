45ff776b021a477aee54d28e6536bfc2695941bc
4eDo <61490588+4eDo@users.noreply.github.com>
2023-12-13 18:38:29 +0300

    Update script.js

--- xmasTree/script.js
+++ xmasTree/script.js
@@ -1,31 +1,48 @@
-function getUsers() {
-	let users;
-	$.ajax({
-		url: 'https://alluvio.ru/api.php',
-		method: 'get',
+const MAX_ID = 1000; // сколько разных id проверять
+
+function getUsers(ids) {
+  var users = [];
+  const myUrlWithParams = new URL("https://alluvio.ru/api.php");
+
+  myUrlWithParams.searchParams.append("method", "users.get");
+  myUrlWithParams.searchParams.append("user_id", ids);
+  myUrlWithParams.searchParams.append("fields", ["user_id", "username", "avatar"]);
+
+  $.ajax({
+		url: myUrlWithParams.href,
+		method: 'post',
 		dataType: 'json',
-		data: {
-			method: "users.get",
-			user_id: [740,741,742,743,744,745,746],
-      fields: ["user_id", "username", "avatar"]
-		},
 		async: false,
 		success: function(data){
-				console.log(data);
-			if(data.error) {
-				console.log("Error on getUsers method");console.log(data);
-			} else {
-	//console.log("getLastInventory invStr = " + data.response.storage.data.backup_inventory);
-        console.log(data);
-				users = data.response.user;
-			}
+      if(data.response.users)
+       users = data.response.users;
 		}
 	});
-	return users;
+  return users;
 } 
 
 $("#loadUsers").click(function() {
-	getUsers();
+	var users = [];
+  
+  let tempArr = [];
+  let tempUsersArr = [];
+  for(let i = 0; i <= MAX_ID; i++) {
+    tempArr.push(i);
+    if(tempArr.length == 100) {
+      tempUsersArr = getUsers(tempArr);
+      console.log(tempUsersArr);
+      users.push(...tempUsersArr);
+      tempArr = [];
+    }
+  }
+  tempUsersArr = getUsers(tempArr);
+  users.push(...tempUsersArr);
+ 
+let ball = `<div class='bauble backgroundAva' style='background-image: url("https://forumavatars.ru{{AVA}}")'> <div class='name'>{{UNAME}}</div></div>`;
+  users.forEach((user, index) => { 
+    $("#treeGrid").append(
+      ball.replace("{{AVA}}", user.avatar).replaceAll('{{UNAME}}', user.username));
+  })
 });
 
 console.log("~~ v 0.01 ~~");
