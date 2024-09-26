462648f410e7b8c2c2a9026c2f30745cbda88e98
4eDo <61490588+4eDo@users.noreply.github.com>
2024-09-26 13:16:53 +0300

    Update main.js

--- potionBook/main.js
+++ potionBook/main.js
@@ -1,4 +1,4 @@
-var potionsSrc= "https://4edo.github.io/alluvio/potionBook/data.json";
+var potionsSrc= "https://4edo.github.io/potions/data.json";
 
 const TEMPLATE_CARD = `<div>
 	<h3>{{NAME}}<sup>{{CANONICAL}}</sup></h3>
